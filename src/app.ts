c67d608e34c413a423f8b247179e2c2d198cf9c2
helong <helong@ciandt.com>
2022-10-13 17:33:30 +0800

    init

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { Component, PropsWithChildren } from 'react'
+import 'taro-ui/dist/style/index.scss'
+
+import './app.scss'
+
+class App extends Component<PropsWithChildren> {
+
+  componentDidMount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  render() {
+    // this.props.children 是将要会渲染的页面
+    return this.props.children
+  }
+}
+
+export default App

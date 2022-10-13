3fffaab718139cbb67ff054652d40ed1f3e593db
Long He <2935842944@qq.com>
2022-10-13 23:21:55 +0800

    modify file

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,44 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    "pages/community/index",
+    "pages/life/index",
+    "pages/housekeeper/index"
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    custom: false,
+    color: "#999999",
+    selectedColor: "#20BA54",
+    backgroundColor: "#fff",
+    position: "bottom",
+    list: [
+      {
+        pagePath: "pages/index/index",
+        text: "首页",
+        iconPath: "images/组 3.png",
+      },
+      {
+        pagePath: "pages/community/index",
+        text: "社区",
+        iconPath: "images/组 114.png",
+      },
+      {
+        pagePath: "pages/life/index",
+        text: "生活",
+        iconPath: "images/组 115.png",
+      },
+      {
+        pagePath: "pages/housekeeper/index",
+        text: "管家",
+
+        iconPath: "images/组 116.png",
+      },
+    ]
   }
 })
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,12 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text, Image, Swiper, SwiperItem } from '@tarojs/components'
-import { AtSearchBar, AtGrid } from 'taro-ui'
+// @import "~taro-ui/dist/style/components/button.scss";
+import { AtSearchBar, AtGrid, AtButton, AtCountdown, AtDivider, AtCard } from 'taro-ui'
 
 import './index.scss'
 import hdbg from '../../images/hdbg.png'
 import testImages from '../../images/img_137.png'
+import sw01bg from '../../images/位图.png'
 
 function Index() {
 
@@ -84,6 +86,140 @@ function Index() {
         }
         />
       </View>
+
+      <View className='swiper01'>
+        <Swiper
+          indicatorColor='#999'
+          indicatorActiveColor='#333'
+          circular
+          previousMargin="16px"
+          nextMargin="12px"
+          displayMultipleItems={1.25}
+        >
+          <SwiperItem>
+            <View className='demo-text-1'>
+              <Image src={sw01bg} ></Image>
+              <View>
+                <Text>成都爆火楼盘2W人登记</Text>
+                <Text>开发商宣讲</Text>
+              </View>
+            </View>
+          </SwiperItem >
+          <SwiperItem>
+            <View className='demo-text-1'>
+              <Image src={sw01bg} ></Image>
+              <View>
+                <Text>成都爆火楼盘2W人登记</Text>
+                <Text>开发商宣讲</Text>
+              </View>
+            </View>
+          </SwiperItem >
+        </Swiper>
+      </View>
+
+      <View className='rmht'>
+        <h2>热门话题</h2>
+        <Text>更多话题&gt;</Text>
+      </View>
+
+      {/*  */}
+      <View className='rm'>
+        <View className='rm-item'>
+          <Image src={sw01bg} ></Image>
+          <Text className='tag'>安静舒适</Text>
+          <Text className='txt'>#居住新方式#</Text>
+          <Text className='popple'>4188人</Text>
+        </View>
+        <View className='rm-item'>
+          <Text className='tag'>安静舒适</Text>
+          <Text className='txt'>#居住新方式#</Text>
+          <Text className='popple'>4188人</Text>
+        </View>
+        <View className='rm-item'>
+          <Text className='tag'>安静舒适</Text>
+          <Text className='txt'>#居住新方式#</Text>
+          <Text className='popple'>4188人</Text>
+        </View>
+      </View>
+
+      {/*  */}
+      <View className='qy'>
+        <View className='left'>
+          <Text>
+            我的『房生活』权益
+          </Text>
+          <Text>
+            快来领取您的专属房生活权益吧～
+          </Text>
+          <AtButton type='secondary' size='small' circle className='btn'>立即激活</AtButton>
+        </View>
+        <View className='right'>
+          <View>
+            <Text>添加我的住房</Text>
+            <Image src={sw01bg} ></Image>
+
+          </View>
+          <View>
+            <Text>添加我的住房</Text>
+            <Image src={sw01bg} ></Image>
+          </View>
+          <View>
+
+            <Image src={sw01bg} ></Image>
+          </View>
+        </View>
+      </View>
+
+      {/*  */}
+      <View className='fjhq'>
+        <View className='left'>
+          <Text>房价行情</Text>
+          <Text>24678</Text>
+        </View>
+        <View className='right'>
+          <Text>房价地图</Text>
+        </View>
+
+      </View>
+
+      <View className='tuangou'>
+        <View className='title'>
+          <Text>
+            <h2>心选好房团购</h2>
+          </Text>
+          <Text>
+            <AtCountdown
+              format={{ hours: ':', minutes: ':', seconds: '' }}
+              seconds={100}
+            />
+            离场结束
+          </Text>
+        </View>
+
+
+      </View>
+
+      <AtCard
+        extra='额外信息'
+        title='这是个标题'
+        thumb='http://www.logoquan.com/upload/list/20180421/logoquan15259400209.PNG'
+      >
+        <View className='content'>
+
+          <View className='left'>
+            <Image src={sw01bg} ></Image>
+
+          </View>
+          <View className='center'>
+
+          </View>
+          <View className='right'></View>
+
+        </View>
+        <AtDivider></AtDivider>
+        <View className='btns'></View>
+        <View className='btn-bottom'></View>
+      </AtCard>
     </>
   )
 }
